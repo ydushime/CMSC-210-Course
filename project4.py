bfe096a65f0ed7debfc565938d132f74f572da6e
Yvon  Dushime <154835836+ydushime@users.noreply.github.com>
2023-12-30 06:57:09 -0500

    Add files via upload

---
+++ project4.py
@@ -0,0 +1,27 @@
+#Project 4
+#CMSC 210
+#Yvon Dushime
+
+#importing the sys module
+import sys
+# read the input from the command
+timesFar = int(sys.argv[1])
+#part 1 of the final scentence
+part_1 = "A long time ago in a galaxy "
+#part 2 of final scentence
+part_2 = "far "
+#part 3 of the final scentence
+part_3 = "away..."
+
+
+    
+#telling what the computer to do if the input is less than 1
+if timesFar < 1:
+    print ("The Universe is vast your view is too small.")
+#telling the computer what to do if the input is greater than 5
+elif timesFar >5:
+    print("Enough already: you're going too far.")
+#telling the computer what to if the inoput is between 1 and 5 and making
+elif 1<= timesFar and timesFar <= 5:
+#printing the final result
+    print(part_1 + part_2 * timesFar + part_3)
